b12bb13b7ed4b9893bec7e27b25af5913f2cfa0e
Bela Novotny <novobacsi@users.noreply.github.com>
2026-09-10 14:29:02 +0200

    remove the no longer needed recursive call

--- go/ledger/ledger.go
+++ go/ledger/ledger.go
@@ -23,11 +23,6 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 	var entriesCopy []Entry
 	entriesCopy = append([]Entry(nil), entries...)
 
-	if len(entries) == 0 {
-		if _, err := FormatLedger(currency, "en-US", []Entry{{Date: "2014-01-01", Description: "", Change: 0}}); err != nil {
-			return "", err
-		}
-	}
 	m1 := map[bool]int{true: 0, false: 1}
 	m2 := map[bool]int{true: -1, false: 1}
 	es := entriesCopy
